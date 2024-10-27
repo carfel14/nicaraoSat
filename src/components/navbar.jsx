5d9e5b1bc568d54a85062b94b7a1e8be84def924
Carlos Ruiz <carfel0609@gmail.com>
2024-10-27 17:28:45 -0600

    Navbar href corrected

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,7 @@ import logo from '@/assets/logo.svg';
 export function Navbar() {
     return (
       <header className="px-4 lg:px-6 h-22 flex items-center border-b border-blue-200 dark:border-blue-800 bg-white dark:bg-gray-800 sticky top-0 z-50">
-        <a className="flex items-center justify-center" href="#">
+        <a className="flex items-center justify-center" href="/">
           <img src={logo} className="h-20 w-20 mr-2 text-[#5386e4] dark:text-blue-400" />
           <span className="font-bold text-2xl text-[#5386e4] dark:text-blue-300">NicaraoSat</span>
         </a>
